2080ba1534e78ead940ec1113d6f6b6343e66744
batonitamazi <tamazi.mirianashvili.1@btu.edu.ge>
2022-10-24 17:26:47 +0400

    modify: assets folder location, added routing, planetPage

--- planets-fact-site/src/App.js
+++ planets-fact-site/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Navbar from './components/header/Navbar';
-
+import data from './data.json';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PlanetPage from './pages/PlanetPage'
 function App() {
   return (
     <div className="App" >
-      <Navbar />
+      <Router>
+        <Navbar data={data} />
+          <Routes>
+            <Route path='/' element={<PlanetPage data={data[0]} />} />
+            {data?.map((element, index) => {
+              return (<Route key={index} path={`${element?.name}`} element={<PlanetPage data={element} />} />)
+            })}
+          </Routes>
+
+      </Router>
+
     </div>
   );
 }
--- planets-fact-site/src/components/header/Navbar.js
+++ planets-fact-site/src/components/header/Navbar.js
@@ -1,19 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import './navbar.css';
 
-function Navbar() {
+function Navbar({data}) {
+  const navigate = useNavigate();
   return (
     <div className='nav'>
-      <h1 className='nav--heading'>The Plannets</h1>
+      <h1 className='nav--heading' onClick={() => navigate('/')}>The Plannets</h1>
       <ul className='nav--list'>
-        <li>Mercury</li>
-        <li>Venus</li>
-        <li>Earth</li>
-        <li>Mars</li>
-        <li>Jupiter</li>
-        <li>Saturn</li>
-        <li>Uranus</li>
-        <li>Neptune</li>
+        {data?.map((element, index) => {
+          return(<li key={index} onClick={() =>  navigate(`/${element.name}`)}>
+            {element.name}
+          </li>)
+        })}
       </ul>
     </div>
   )
